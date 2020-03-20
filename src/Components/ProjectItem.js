6d3f72fa2e2d209bc0f29024d4fecb0dbfea7230
Davit Sahakyan <sahakyandavit92@gmail.com>
2020-03-20 19:29:10 +0400

    Remove :(ProjectItem) some projects

--- src/Components/ProjectItem.js
+++ src/Components/ProjectItem.js
@@ -1,332 +1,115 @@
-import React from 'react'
-import { Card , CardTitle ,CardActions ,CardMenu , Button , IconButton , CardText} from 'react-mdl'
+import React from "react";
+import {
+    Card,
+    CardTitle,
+    CardActions,
+    CardMenu,
+    Button,
+    IconButton,
+    CardText
+} from "react-mdl";
 
-
-
-const ProjectItem = () =>{
-  return (
-    <div
-      className="project-items"
-      style={{
-        display: "flex",
-        flexWrap: "wrap ",
-        justifyContent: "space-between"
-      }}
-    >
-      {/* Project1*/}
-      <Card
-        shadow={5}
-        style={{ minWidth: "450", margin: "auto", padding: "10px" }}
-      >
-        <a
-          href="https://davitsahakyan.github.io/honey/"
-          target="_blank"
-          rel="noopener noreferrer"
+const ProjectItem = () => {
+    return (
+        <div
+            className="project-items"
+            style={{
+                display: "flex",
+                flexWrap: "wrap ",
+                justifyContent: "space-between"
+            }}
         >
-          <CardTitle
-            className="bg-image1"
-            style={{ color: "#fff", height: "176px" }}
-          >
-            {" "}
-            React Project #1
-          </CardTitle>{" "}
-        </a>
-
-        <CardText>
-          Էջը Ստեղծվել է Դավիթ Սահակյանի կողմից և նախատեսված է որպես
-          տեղեկատվական հարթակ ծառայելու բոլոր մեղր օգտագործողներին:
-        </CardText>
-
-        <CardActions border>
-          <Button colored>
-            {" "}
-            <a
-              href="https://github.com/davitSahakyan/honey"
-              target="_blank"
-              rel="noopener noreferrer"
+            {/* Project1*/}
+            <Card
+                shadow={5}
+                style={{ minWidth: "450", margin: "auto", padding: "10px" }}
             >
-              {" "}
-              GitHub{" "}
-            </a>{" "}
-          </Button>
-          <Button colored> Codepen</Button>
-          <Button colored> LiveDemo</Button>
-        </CardActions>
+                <a
+                    href="https://davitsahakyan.github.io/honey/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <CardTitle
+                        className="bg-image1"
+                        style={{ color: "#fff", height: "176px" }}
+                    >
+                        {" "}
+                        React Project #1
+                    </CardTitle>{" "}
+                </a>
 
-        <CardMenu style={{ color: "#fff" }}>
-          <IconButton name="share" />
-        </CardMenu>
-      </Card>
+                <CardText>
+                    Էջը Ստեղծվել է Դավիթ Սահակյանի կողմից և նախատեսված է որպես
+                    տեղեկատվական հարթակ ծառայելու բոլոր մեղր օգտագործողներին:
+                </CardText>
 
-      {/* Project2*/}
-      <Card
-        shadow={5}
-        style={{ minWidth: "450", margin: "auto", padding: "10px" }}
-      >
-        <a
-          href="https://davitsahakyan.github.io/Crypto/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <CardTitle
-            className="bg-image2"
-            style={{ color: "#fff", height: "176px" }}
-          >
-            {" "}
-            React Project #2
-          </CardTitle>{" "}
-        </a>
+                <CardActions border>
+                    <Button colored>
+                        {" "}
+                        <a
+                            href="https://github.com/davitSahakyan/honey"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            {" "}
+                            GitHub{" "}
+                        </a>{" "}
+                    </Button>
+                    <Button colored> Codepen</Button>
+                    <Button colored> LiveDemo</Button>
+                </CardActions>
 
-        <CardText>
-          Էջը Ստեղծվել է Դավիթ Սահակյանի կողմից և կծանոթացնի ինքնաբավարար թվային
-          արժույթի տեսակներին ինչպիսին են Բիթքոինը և այլն:
-        </CardText>
+                <CardMenu style={{ color: "#fff" }}>
+                    <IconButton name="share" />
+                </CardMenu>
+            </Card>
 
-        <CardActions border>
-          <Button colored>
-            {" "}
-            <a
-              href="https://github.com/davitSahakyan/Crypto"
-              target="_blank"
-              rel="noopener noreferrer"
+            {/* Project2*/}
+            <Card
+                shadow={5}
+                style={{ minWidth: "450", margin: "auto", padding: "10px" }}
             >
-              GitHub
-            </a>
-          </Button>
-          <Button colored> Codepen</Button>
-          <Button colored> LiveDemo</Button>
-        </CardActions>
+                <a
+                    href="https://davitsahakyan.github.io/Crypto/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <CardTitle
+                        className="bg-image2"
+                        style={{ color: "#fff", height: "176px" }}
+                    >
+                        {" "}
+                        React Project #2
+                    </CardTitle>{" "}
+                </a>
 
-        <CardMenu style={{ color: "#fff" }}>
-          <IconButton name="share" />
-        </CardMenu>
-      </Card>
+                <CardText>
+                    Էջը Ստեղծվել է Դավիթ Սահակյանի կողմից և կծանոթացնի
+                    ինքնաբավարար թվային արժույթի տեսակներին ինչպիսին են Բիթքոինը
+                    և այլն:
+                </CardText>
 
-      {/* Project3*/}
-      <Card
-        shadow={5}
-        style={{ minWidth: "450", margin: "auto", padding: "10px" }}
-      >
-        <a
-          href="https://react-phone-store-recorded.netlify.com/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <CardTitle
-            className="bg-image3"
-            style={{ color: "#009fff", height: "176px" }}
-          >
-            {" "}
-            React Project #3
-          </CardTitle>{" "}
-        </a>
+                <CardActions border>
+                    <Button colored>
+                        {" "}
+                        <a
+                            href="https://github.com/davitSahakyan/Crypto"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            GitHub
+                        </a>
+                    </Button>
+                    <Button colored> Codepen</Button>
+                    <Button colored> LiveDemo</Button>
+                </CardActions>
 
-        <CardText>
-          Էջը Ստեղծվել է Դավիթ Սահակյանի կողմից և նախատեսված է որպես բջջային
-          հեռախոսների վաճառքի օնլայն հարթակ:
-        </CardText>
+                <CardMenu style={{ color: "#fff" }}>
+                    <IconButton name="share" />
+                </CardMenu>
+            </Card>
+        </div>
+    );
+};
 
-        <CardActions border>
-          <Button colored>
-            {" "}
-            <a
-              href="https://github.com/davitSahakyan/react-phone-store"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              {" "}
-              GitHub{" "}
-            </a>{" "}
-          </Button>
-          <Button colored> Codepen</Button>
-          <Button colored> LiveDemo</Button>
-        </CardActions>
-
-        <CardMenu style={{ color: "#fff" }}>
-          <IconButton name="share" />
-        </CardMenu>
-      </Card>
-
-      {/* Project4*/}
-      <Card
-        shadow={5}
-        style={{ minWidth: "450", margin: "auto", padding: "10px" }}
-      >
-        <a
-          href="https://davitsahakyan.github.io/anime-test-2/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <CardTitle
-            className="bg-image4"
-            style={{ color: "#009fff", height: "176px" }}
-          >
-            {" "}
-            React Project #4
-          </CardTitle>{" "}
-        </a>
-
-        <CardText>
-          Թեստը Ստեղծվել է Դավիթ Սահակյանի կողմից և այն վերաբերում է Ճապոնիայում
-          ստեղծված անիմացիաներին կամ ուրիշ կերպ ասած Անիմեին:
-        </CardText>
-
-        <CardActions border>
-          <Button colored>
-            {" "}
-            <a
-              href="https://github.com/davitSahakyan/anime-test-2"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              {" "}
-              GitHub{" "}
-            </a>{" "}
-          </Button>
-          <Button colored> Codepen</Button>
-          <Button colored> LiveDemo</Button>
-        </CardActions>
-
-        <CardMenu style={{ color: "#fff" }}>
-          <IconButton name="share" />
-        </CardMenu>
-      </Card>
-
-      {/* Project5*/}
-      <Card
-        shadow={5}
-        style={{ minWidth: "450", margin: "auto", padding: "10px" }}
-      >
-        <a
-          href="https://davitsahakyan.github.io/recipes"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          {" "}
-          <CardTitle
-            className="bg-image5"
-            style={{ color: "#009fff", height: "176px" }}
-          >
-            {" "}
-            React Project #5{" "}
-          </CardTitle>{" "}
-        </a>
-
-        <CardText>
-          Նշեք այն բաղադրիչը ինչով ցանկանում եք սնունդ պատրաստել և ստացեք
-          ապուրների պատրաստման բաղադրատոմսեր:
-        </CardText>
-
-        <CardActions border>
-          <Button colored>
-            {" "}
-            <a
-              href="https://github.com/davitSahakyan/recipes"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              {" "}
-              GitHub{" "}
-            </a>{" "}
-          </Button>
-          <Button colored> Codepen</Button>
-          <Button colored> LiveDemo</Button>
-        </CardActions>
-
-        <CardMenu style={{ color: "#fff" }}>
-          <IconButton name="share" />
-        </CardMenu>
-      </Card>
-
-      {/* Project6*/}
-      <Card
-        shadow={5}
-        style={{ minWidth: "450", margin: "auto", padding: "10px" }}
-      >
-        <a
-          href="https://davitsahakyan.github.io/snakegame/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          {" "}
-          <CardTitle
-            className="bg-image6"
-            style={{ color: "#009fff", height: "176px" }}
-          >
-            {" "}
-            React Project #6{" "}
-          </CardTitle>{" "}
-        </a>
-
-        <CardText>Խաղ Օձ հավաքված React -ով դեռ կատարելագործվում է:</CardText>
-
-        <CardActions border>
-          <Button colored>
-            {" "}
-            <a
-              href="https://github.com/davitSahakyan/snakegame"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              {" "}
-              GitHub{" "}
-            </a>{" "}
-          </Button>
-          <Button colored> Codepen</Button>
-          <Button colored> LiveDemo</Button>
-        </CardActions>
-
-        <CardMenu style={{ color: "#fff" }}>
-          <IconButton name="share" />
-        </CardMenu>
-      </Card>
-       {/* Project7*/}
-       <Card
-        shadow={5}
-        style={{ minWidth: "450", margin: "auto", padding: "10px" }}
-      >
-        <a
-          href="https://davitsahakyan.github.io/todo"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          {" "}
-          <CardTitle
-            className="bg-image7"
-            style={{ color: "#009fff", height: "176px" }}
-          >
-            {" "}
-            React Project #7{" "}
-          </CardTitle>{" "}
-        </a>
-
-        <CardText>
-          Կատարելիք գործերի ցուցակ: Ցուցակում կարղ եք ավելացնել ջնջձլ կամ ֆիլտրել ցանկը: 
-        </CardText>
-
-        <CardActions border>
-          <Button colored>
-            {" "}
-            <a
-              href="https://github.com/davitSahakyan/todo"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              {" "}
-              GitHub{" "}
-            </a>{" "}
-          </Button>
-          <Button colored> Codepen</Button>
-          <Button colored> LiveDemo</Button>
-        </CardActions>
-
-        <CardMenu style={{ color: "#fff" }}>
-          <IconButton name="share" />
-        </CardMenu>
-      </Card>
-    </div>
-  );
-}
-
-
-export default ProjectItem
+export default ProjectItem;
