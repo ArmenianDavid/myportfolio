5a5450801849e19094d8039d6009892ede4e21a9
Davit Sahakyan <sahakyandavit92@gmail.com>
2020-03-31 20:25:38 +0400

    Refactor formatted by prettier , deleted some components and folders

--- src/App.js
+++ src/App.js
@@ -1,35 +1,54 @@
-import React from 'react';
-import './App.css';
-import {Layout,Header,Navigation,Drawer,Content} from 'react-mdl'
-import Main from './Components/main'
-import {Link } from 'react-router-dom'
+import React from "react";
+import "./App.css";
+import { Layout, Header, Navigation, Drawer, Content } from "react-mdl";
+import Main from "./Components/main";
+import { Link } from "react-router-dom";
 
-
-const App = () =>{
-  return(
-    <div className="demo-big-content">
-    <Layout>
-        <Header className='header-color' title={<Link style={{textDecoration:'none' , color:'white'}} to="/myportfolio">MyPortfolio</Link>} scroll>
-            <Navigation>
-                <Link to="/resume">Resume</Link>
-                <Link to="/projects">Projects</Link>
-                <Link to="/contact">Contacts</Link>
-            </Navigation>
-        </Header>
-        <Drawer title={<Link style={{textDecoration:'none' , color:'blue'}} to="/myportfolio">MyPortfolio</Link>}>
-            <Navigation>
-                <Link to="/resume">Resume</Link>
-                <Link to="/projects">Projects</Link>
-                <Link to="/contact">Contacts</Link>
-            </Navigation>
-        </Drawer>
-        <Content>
-            <div className="page-content" />
-            <Main />
-        </Content>
-    </Layout>
-</div>
-  )
-}
+const App = () => {
+    return (
+        <div className="demo-big-content">
+            <Layout>
+                <Header
+                    className="header-color"
+                    title={
+                        <Link
+                            style={{ textDecoration: "none", color: "white" }}
+                            to="/myportfolio"
+                        >
+                            MyPortfolio
+                        </Link>
+                    }
+                    scroll
+                >
+                    <Navigation>
+                        <Link to="/resume">Resume</Link>
+                        <Link to="/projects">Projects</Link>
+                        <Link to="/contact">Contacts</Link>
+                    </Navigation>
+                </Header>
+                <Drawer
+                    title={
+                        <Link
+                            style={{ textDecoration: "none", color: "blue" }}
+                            to="/myportfolio"
+                        >
+                            MyPortfolio
+                        </Link>
+                    }
+                >
+                    <Navigation>
+                        <Link to="/resume">Resume</Link>
+                        <Link to="/projects">Projects</Link>
+                        <Link to="/contact">Contacts</Link>
+                    </Navigation>
+                </Drawer>
+                <Content>
+                    <div className="page-content" />
+                    <Main />
+                </Content>
+            </Layout>
+        </div>
+    );
+};
 
 export default App;
--- src/Components/main.js
+++ src/Components/main.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Landing from "./landingpage";
 import { Switch, Route } from "react-router-dom";
-import Aboutme from "./aboutme";
 import Contact from "./contact";
 import Projects from "./projects";
 import Resume from "./resume";
@@ -10,7 +9,6 @@ const Main = () => {
     return (
         <Switch>
             <Route exact path="/myportfolio" component={Landing} />
-            <Route path="/aboutme" component={Aboutme} />
             <Route path="/contact" component={Contact} />
             <Route path="/projects" component={Projects} />
             <Route path="/resume" component={Resume} />
