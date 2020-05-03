bd3223fd13934d7a969265dc876e738c5287e8a6
Davit Sahakyan <sahakyandavit92@gmail.com>
2020-05-03 20:17:49 +0400

    Add Hotel Rooms project in react projects data

--- src/Components/Data/reactProjectsData.js
+++ src/Components/Data/reactProjectsData.js
@@ -1,6 +1,7 @@
 // images
 import blog from "../../img/blog.png";
 import honey from "../../img/honey.png";
+import hotelRooms from "../../img/hotelRooms.png";
 
 export const reactProjectsData = [
     {
@@ -8,13 +9,20 @@ export const reactProjectsData = [
         demoLink: "https://davitsahakyan.github.io/simple-blog/#/simple-blog",
         img: blog,
         description: `Blog is created by Davit Sahakyan using tools like MaterialUi , LocalStorage.`,
-        github: "https://github.com/davitSahakyan/simple-blog"
+        github: "https://github.com/davitSahakyan/simple-blog",
     },
     {
         id: 2,
         demoLink: "https://davitsahakyan.github.io/honey/",
         img: honey,
         description: `Honey is nature's finest work. Discover honey recipes, where to buy honey, information about honeybees, and resources for honey professionals.`,
-        github: "https://github.com/davitSahakyan/honey"
-    }
+        github: "https://github.com/davitSahakyan/honey",
+    },
+    {
+        id: 3,
+        demoLink: "https://react-hotel-rooms.netlify.app",
+        img: hotelRooms,
+        description: `In this site you can search for a room that matches to your needs.`,
+        github: "https://github.com/davitSahakyan/hotel-rooms",
+    },
 ];
