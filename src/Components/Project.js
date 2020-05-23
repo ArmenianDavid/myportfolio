2c1459d3f791de26a5ea5fe4e9d8691665e55eca
Davit Sahakyan <sahakyandavit92@gmail.com>
2020-05-23 21:48:57 +0400

    Style fix projects position and size

--- src/Components/Project.js
+++ src/Components/Project.js
@@ -6,33 +6,39 @@ import {
     CardMenu,
     Button,
     IconButton,
-    CardText
+    CardText,
 } from "react-mdl";
 
 export default function Project({ id, demoLink, img, description, github }) {
+    const cardTitleStyle = {
+        color: "#fff",
+        height: "176px",
+        backgroundImage: `url(${img})`,
+        backgroundPosition: "center",
+        backgroundSize: "cover",
+    };
+
     return (
         <Card
             shadow={5}
-            style={{ minWidth: "450", margin: "auto", padding: "10px" }}
+            style={{
+                minWidth: "450",
+                maxWidth: "450",
+                margin: "5px",
+                padding: "10px",
+            }}
         >
             <a href={demoLink} target="_blank" rel="noopener noreferrer">
                 <CardTitle
                     className="bg-image2"
-                    style={{
-                        color: "#fff",
-                        height: "176px",
-                        backgroundImage: `url(${img})`,
-                        backgroundPosition: "center",
-                        backgroundSize: "cover"
-                    }}
-                ></CardTitle>{" "}
+                    style={cardTitleStyle}
+                ></CardTitle>
             </a>
 
             <CardText>{description}</CardText>
 
             <CardActions border>
                 <Button colored>
-                    {" "}
                     <a href={github} target="_blank" rel="noopener noreferrer">
                         GitHub
                     </a>
