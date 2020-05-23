9347d58aa220a259bbaa923b7e9a32b437c85ac4
Davit Sahakyan <sahakyandavit92@gmail.com>
2020-05-23 21:55:37 +0400

    Add recipe project

--- src/Components/ProjectsReact.js
+++ src/Components/ProjectsReact.js
@@ -3,16 +3,15 @@ import Project from "./Project";
 import { reactProjectsData } from "./Data/ReactProjectsData.js";
 
 const ProjectItem = () => {
+    const style = {
+        display: "flex",
+        flexWrap: "wrap ",
+        justifyContent: "center",
+    };
+
     return (
-        <div
-            className="project-items"
-            style={{
-                display: "flex",
-                flexWrap: "wrap ",
-                justifyContent: "space-between"
-            }}
-        >
-            {reactProjectsData.map(item => {
+        <div className="project-items" style={style}>
+            {reactProjectsData.map((item) => {
                 return (
                     <Project
                         key={item.id}
